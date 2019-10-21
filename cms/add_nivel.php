f1e85ac91ef6f99d3349a1272566944195b48e7e
mtzzwr <mateus.gsep@gmail.com>
2019-10-21 14:19:33 -0300

    controle de niveis e usuários finalizado

--- cms/add_nivel.php
+++ cms/add_nivel.php
@@ -144,7 +144,7 @@ if (isset($_POST['btn-cadastrar'])) {
         }
 
 
-        $sql = "INSERT INTO tbl_nivel VALUES (null, '" . $nome . "', '" . $desc . "', " . $chkConteudo . ", " . $chkFaleConosco . ", " . $chkUsuario . ", " . $status . ", null)";
+        $sql = "INSERT INTO tbl_nivel VALUES (null, '" . $nome . "', '" . $desc . "', " . $chkConteudo . ", " . $chkFaleConosco . ", " . $chkUsuario . ", " . $status . ")";
 
         if (mysqli_query($conexao, $sql)) echo 'funfou';
         else echo $sql;
