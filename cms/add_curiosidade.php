39a30fbad69694641e6d5c42ea2f6ccb58069f19
mtzzwr <mateus.gsep@gmail.com>
2019-11-02 10:53:28 -0300

    arquivo de upload de imagem adicionado

--- cms/add_curiosidade.php
+++ cms/add_curiosidade.php
@@ -12,14 +12,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./css/template.css">
-    <title>Conteúdo - CMS</title>
+    <link rel="stylesheet" href="./css/add_curiosidade.css">
+    <title>Adicionar Curiosidade - CMS</title>
 </head>
 
 <body>
     <?php include './include/header.php'; ?>
     <section class="conteudo-principal">
-        <h1>Conteúdos da página Curiosidades</h1>
-        <a href="add_curiosidade.php">Nova Curiosidade</a>
+        <h1>Nova Curiosidade</h1>
+        <form class="form-template" action="" method="post" enctype="multipart/form-data">
+            Escolha uma imagem
+            <label id="thumbnail">
+                <input type="file" />
+                <img src="./images/camera.png" alt="Select img" />
+            </label>
+        </form>
     </section>
     <?php include './include/footer.php'; ?>
 </body>
---
+++ cms/db/upload_imagem.php
@@ -0,0 +1,43 @@
+<?php 
+
+function uploadImagem($file_name){
+    
+    $dir = (string) 'files/';
+    $max_file_size = (int) 1000;
+    $accepted_files = array('image/jpeg', 'image/png', 'image/jpg');
+
+    if($_FILES['foto']['type'] != ''){
+        if($_FILES['foto']['size'] > 0){
+            $type_file = $_FILES['foto']['type'];
+
+            if(in_array($type_file, $accepted_files)){
+                $file_size = round($_FILES['foto']['size'] / 1024);
+
+                if($file_size <= $max_file_size){
+                    $real_file_name = $_FILES['foto']['name'];
+                    $file_name = pathinfo($real_file_name, PATHINFO_FILENAME);
+                    $file_extension = pathinfo($real_file_name, PATHINFO_EXTENSION);
+
+                    $crypt_file_name = md5(uniqid(time() . $file_name));
+                    $image_name = $crypt_file_name . '.' . $file_extension;
+
+                    $tmp_file = $_FILES['foto']['tmp_name'];
+
+                    if(move_uploaded_file($tmp_file, $dir . $image_name)){
+                        return $image_name;
+                    }else{
+                        echo '<script>alert("Erro ao enviar o arquivo para o server");</script>';
+                    }
+                }else{
+                    echo '<script>alert("Tamanho do arquivo maior que o permitido - (1mb)");</script>';
+                }
+            }else{
+                echo '<script>alert("Tipo do arquivo não é permitido");</script>';
+            }
+        }else{
+            echo '<script>alert("O tamanho do arquivo é inexistente");</script>';
+        }
+    }else{
+        echo '<script>alert("Tipo do arquivo está vazio");</script>';
+    }
+}
