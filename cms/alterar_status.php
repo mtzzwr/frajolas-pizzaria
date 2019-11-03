abbfa2920f7ee19f182be85667f910d8495937b4
mtzzwr <mateus.gsep@gmail.com>
2019-11-03 08:38:24 -0300

    melhoria no código de alterar status

--- cms/alterar_status.php
+++ cms/alterar_status.php
@@ -13,83 +13,30 @@ $st = $_GET['status'];
 
 if (isset($alterar)) {
     if ($alterar == 'usuario') {
-        $sql = "select * from tbl_usuario where id_usuario = " . $cod . "";
-
-        $select_all = mysqli_query($conexao, $sql);
-
-        while ($rs = mysqli_fetch_array($select_all)) {
-            $status = $rs['status'];
-        }
-
-        if ($st == 0) {
-            $sql = "UPDATE tbl_usuario SET status = 1 where id_usuario = " . $cod . "";
-
-            if ($select = mysqli_query($conexao, $sql)) {
-                header('location:./controle_usuario.php');
-            } else {
-                echo 'erro';
-            }
-        } else if ($st == 1) {
-            $sql = "UPDATE tbl_usuario SET status = 0 where id_usuario = " . $cod . "";
-
-            if ($select = mysqli_query($conexao, $sql)) {
-                header('location:./controle_usuario.php');
-            } else {
-                echo 'erro';
-            }
-        }
+        $nome_tabela = 'tbl_usuario';
+        $id_tabela = 'id_usuario';
     } else if ($alterar == 'nivel') {
-
-        $sql = "select * from tbl_nivel where id_nivel = " . $cod . "";
-
-        $select_all = mysqli_query($conexao, $sql);
-
-        while ($rs = mysqli_fetch_array($select_all)) {
-            $status = $rs['status'];
-        }
-
-        if ($st == 0) {
-            $sql = "UPDATE tbl_nivel SET status = 1 WHERE id_nivel = " . $cod . "";
-
-            if ($select = mysqli_query($conexao, $sql)) {
-                header('location:./controle_niveis.php');
-            } else {
-                echo 'erro';
-            }
-        } else if ($st == 1) {
-            $sql = "UPDATE tbl_nivel SET status = 0 WHERE id_nivel = " . $cod . "";
-
-            if ($select = mysqli_query($conexao, $sql)) {
-                header('location:./controle_niveis.php');
-            } else {
-                echo 'erro';
-            }
-        }
+        $nome_tabela = 'tbl_nivel';
+        $id_tabela = 'id_nivel';
     }else if($alterar == 'curiosidade'){
-        $sql = "select * from tbl_curiosidade where id_curiosidade = " . $cod . "";
-
-        $select_all = mysqli_query($conexao, $sql);
-
-        while ($rs = mysqli_fetch_array($select_all)) {
-            $status = $rs['status'];
-        }
-
-        if($st == 0){
-            $sql = "UPDATE tbl_curiosidade SET status = 1 WHERE id_curiosidade = " . $cod . "";
-
-            if ($select = mysqli_query($conexao, $sql)) {
-                header('location:./conteudo_curiosidades.php');
-            } else {
-                echo 'erro';
-            }
-        }else if($st == 1){
-            $sql = "UPDATE tbl_curiosidade SET status = 0 WHERE id_curiosidade = " . $cod . "";
-
-            if ($select = mysqli_query($conexao, $sql)) {
-                header('location:./conteudo_curiosidades.php');
-            } else {
-                echo 'erro';
-            } 
-        }
+        $nome_tabela = 'tbl_curiosidade';
+        $id_tabela = 'id_curiosidade';
     }
+
+    $sql = "SELECT * FROM ".$nome_tabela." WHERE ".$id_tabela." = ".$cod;
+
+    $select = mysqli_query($conexao, $sql);
+
+    while($rs = mysqli_fetch_array($select)){
+        $status = $rs['status'];
+    }
+
+    if ($st == 0) $sql = "UPDATE ".$nome_tabela." SET status = 1 where ". $id_tabela." = " . $cod . ""; 
+    else if ($st == 1) $sql = "UPDATE ".$nome_tabela." SET status = 0 where ". $id_tabela." = " . $cod . "";
+
+    if ($select = mysqli_query($conexao, $sql)) echo '<script>window.history.go(-1);</script>';
+    else echo $sql;
+
 }
+
+
